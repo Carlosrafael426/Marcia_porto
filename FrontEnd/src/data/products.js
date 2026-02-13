1c4f3142412afeaf6e13b9138cfa82f4a1355aa7
carlosrafael426 <carlosrafael360@gmail.com>
2026-02-13 14:14:49 -0300

    ultimas atualizações FrontEnd

--- FrontEnd/src/data/products.js
+++ FrontEnd/src/data/products.js
@@ -1,52 +1,86 @@
+import boloChantilly from "../assets/Bolos/bolo_tema_whatsapp_image_2026_01_26_at_14_48_24_2.jpeg"
+import boloChantininho from "../assets/Bolos/bolo_tema_whatsapp_image_2026_01_26_at_16_09_18_3.jpeg"
+import boloPastaAmericana from "../assets/Bolos/bolo_tema_whatsapp_image_2026_01_26_at_15_57_24_1.jpeg"
+import boloTematico from "../assets/Bolos/bolo_tema_natal.jpg"
+import docePersonalizado from "../assets/Doces/bolo_tema_whatsapp_image_2026_01_26_at_16_17_01_4.jpeg"
+import brigadeiroGourmet from "../assets/Doces/bri_gourmet.jpg"
+import brigadeiroRecheado from "../assets/Doces/bri_gourmet1.jpg"
+import brigadeiroTradicional from "../assets/Doces/briga_tradicional.jpg"
+import kitFesta from "../assets/Doces/kit_festa.jpg"
+
 export const categories = ["Bolos", "Doces", "Personalizados"];
 
 export const products = [
   {
     id: 1,
-    name: "Bolo de Chocolate",
+    name: "Bolo de Chantily",
     category: "Bolos",
-    priceFrom: "R$ 80",
+    priceFrom: "R$ 100,00kg",
     desc: "Massa fofinha, recheio cremoso e cobertura caprichada.",
-    img: "https://images.unsplash.com/photo-1541783245831-57d6fb0926d3?auto=format&fit=crop&w=1200&q=80",
+    img: boloChantilly,
   },
   {
     id: 2,
-    name: "Bolo de Cenoura",
+    name: "Bolo de Chantininho",
     category: "Bolos",
-    priceFrom: "R$ 75",
-    desc: "Com cobertura de chocolate — clássico que sempre vende.",
-    img: "https://images.unsplash.com/photo-1603532648955-039310d9ed75?auto=format&fit=crop&w=1200&q=80",
+    priceFrom: "R$ 115,00kg",
+    desc: "Com cobertura de Chantininho — clássico delicioso.",
+    img: boloChantininho,
   },
   {
     id: 3,
-    name: "Brigadeiros Gourmet",
-    category: "Doces",
-    priceFrom: "R$ 3,50/un",
-    desc: "Sabores variados, perfeito pra festas e presentes.",
-    img: "https://images.unsplash.com/photo-1614707267537-2b9e2c1a01f8?auto=format&fit=crop&w=1200&q=80",
+    name: "Bolo de Pasta Americana",
+    category: "Bolos",
+    priceFrom: "R$ 160,00kg",
+    desc: "Com cobertura de pasta americana — clássico que sempre vende.",
+    img: boloPastaAmericana,
   },
   {
     id: 4,
-    name: "Beijinho",
+    name: "Brigadeiros tradicionais",
     category: "Doces",
-    priceFrom: "R$ 3,50/un",
-    desc: "Cremoso e equilibrado, finalizado com carinho.",
-    img: "https://images.unsplash.com/photo-1616699002805-0741e1e4a9c5?auto=format&fit=crop&w=1200&q=80",
+    priceFrom: "R$ 100,00 /cento",
+    desc: "Sabores variados, perfeito pra festas e presentes.",
+    img: brigadeiroTradicional,
   },
   {
     id: 5,
+    name: "Brigadeiros Gourmet",
+    category: "Doces",
+    priceFrom: "R$ 150,00 /cento",
+    desc: "Sabores variados, perfeito pra festas e presentes confirmar sabores disponiveis.",
+    img: brigadeiroGourmet,
+  },
+  {
+    id: 6,
+    name: "Brigadeiros Recheados",
+    category: "Doces",
+    priceFrom: "R$ 180,00 /cento",
+    desc: "Sabores variados, perfeito pra festas e presentes confirmar recheios disponiveis.",
+    img: brigadeiroRecheado,
+  },
+  {
+    id: 7,
+    name: "Doces Finos",
+    category: "Doces",
+    priceFrom: "R$ 250,00 /cento",
+    desc: "Cremoso e equilibrado, finalizado com carinho confirmar os doces disponiveis.",
+    img: docePersonalizado,
+  },
+  {
+    id: 8,
     name: "Bolo Temático",
     category: "Personalizados",
     priceFrom: "Sob consulta",
     desc: "Personalização por tema, cores e acabamento.",
-    img: "https://images.unsplash.com/photo-1523293836415-64e58a1c7e1a?auto=format&fit=crop&w=1200&q=80",
+    img: boloTematico,
   },
   {
-    id: 6,
+    id: 9,
     name: "Kit Festa",
     category: "Personalizados",
     priceFrom: "Sob consulta",
-    desc: "Combos com bolo + doces para sua comemoração.",
-    img: "https://images.unsplash.com/photo-1608198093002-ad4e005484ec?auto=format&fit=crop&w=1200&q=80",
+    desc: "Combos com bolo + doces para sua comemoração(Não tabalhamos com decoração, trabalhamos apenas com doces e bolos).",
+    img: kitFesta,
   },
 ];
